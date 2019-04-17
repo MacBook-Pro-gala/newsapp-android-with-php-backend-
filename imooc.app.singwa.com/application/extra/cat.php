d47b9be013f049282fbdb5fe1e6df716ff62c903
刘宇涵 <lyhgz@outlook.com>
2019-04-17 16:14:18 +0800

    更新

---
+++ imooc.app.singwa.com/application/extra/cat.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baidu
+ * Date: 17/7/31
+ * Time: 上午1:51
+ */
+
+return [
+    'lists' => [
+        1 => '工学',
+        2 => '理学',
+        3 => '文学',
+        4 => '艺术'
+    ],
+];
